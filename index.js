4918088621099f5e72603ff20686327e304d5625
aliasghernooruddin <aliasghernooruddin@gmail.com>
2022-06-02 00:22:42 +0500

    file conversion and upload to s3 done

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const dotenv = require('dotenv');
-var AWS = require('aws-sdk');
-var path = require('path');
-
+const AWS = require('aws-sdk');
 const utils = require("./utils.js")
 
-dotenv.config();
-
-// console.log(process.env.AWS_ACCESS_KEY)
-
 exports.handler = async (event, context) => {
-    console.log("Number of records in this event is ", event.Records.length);
 
-    for (const element of event.Records) {
+    let length = event.Records.length < 10 ? event.Records.length : 10
+    records = event.Records.slice(0, 5)
+
+    for (const element of records) {
         var marshalled = AWS.DynamoDB.Converter.unmarshall(element.dynamodb.NewImage);
         if (element.eventName == 'INSERT' && marshalled.CertStatus == "pending") {
-            await utils.modifyTemplate('cerTemp.pptx', marshalled.ArrData[0]['certsdata'])
             await utils.downloadTemplate(marshalled.eventCertTemplateURL)
+            await utils.modifyTemplate('cerTemp.pptx', marshalled.ArrData[0]['certsdata'])
+            let url = await utils.cloudConvertToPDF('output.pptx')
+            await utils.uploadToS3(url, marshalled.ArrData[0]['generalinfo'], marshalled.ArrData[0]['certsdata'].UserName)
         }
         else if (element.eventName == 'MODIFY' && element.CertStatus == "generated") {
 
--- utils.js
+++ utils.js
@@ -1,8 +1,28 @@
 const http = require('http');
+const https = require('https');
 const fs = require('fs');
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
 const path = require("path");
+const CloudConvert = require('cloudconvert');
+const dotenv = require('dotenv');
+const AWS = require('aws-sdk'); // not needed on lambda 
+const axios = require('axios').default;
+
+dotenv.config();
+
+const CLOUD_CONVERT_API_KEY = process.env.CLOUD_CONVERT_KEY
+const AWS_REGION = process.env.AWS_REGION
+const S3_BUCKET = process.env.S3_BUCKET
+
+const cloudConvert = new CloudConvert(CLOUD_CONVERT_API_KEY);
+
+//------ s3 config (not needed on labmda )
+const s3 = new AWS.S3({ apiVersion: '2006-03-01', region: AWS_REGION });
+s3.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+});
 
 const downloadTemplate = function downloadFileUsingS3(link) {
 
@@ -21,39 +41,159 @@ const downloadTemplate = function downloadFileUsingS3(link) {
     })
 }
 
-
 const modifyTemplate = function modifyTemplate(filename, data) {
+    return new Promise(resolve => {
+        // Load the docx file as binary content
+        const content = fs.readFileSync(
+            path.resolve(__dirname, filename),
+            "binary"
+        );
 
-    // Load the docx file as binary content
-    const content = fs.readFileSync(
-        path.resolve(__dirname, filename),
-        "binary"
-    );
+        const zip = new PizZip(content);
 
-    const zip = new PizZip(content);
+        const doc = new Docxtemplater(zip, {
+            paragraphLoop: true,
+            linebreaks: true,
+        });
 
-    const doc = new Docxtemplater(zip, {
-        paragraphLoop: true,
-        linebreaks: true,
-    });
+        doc.render({
+            UserName: data.UserName,
+            WebinarName: data.WebinarName,
+            Duration: data.Duration,
+            Date: data.Date,
+        });
 
-    doc.render({
-        UserName: data.UserName,
-        WebinarName: data.WebinarName,
-        Duration: data.Duration,
-        Date: data.Date,
-    });
-
-    const buf = doc.getZip().generate({
-        type: "nodebuffer",
-        compression: "DEFLATE",
-    });
+        const buf = doc.getZip().generate({
+            type: "nodebuffer",
+            compression: "DEFLATE",
+        });
 
 
-    fs.writeFileSync(path.resolve(__dirname, "output.pptx"), buf);
+        fs.writeFileSync(path.resolve(__dirname, "output.pptx"), buf);
+        resolve()
+    })
 }
 
+const cloudConvertToPDF = function cloudConvertToPDF(File) {
+
+    return new Promise(async function (resolve, reject) {
+        await cloudConvert.jobs.create({
+            "tasks": {
+                'upload-my-file': {
+                    operation: 'import/upload'
+                },
+                "Convert2pdf": {
+                    "operation": "convert",
+                    "input": [
+                        "upload-my-file"
+                    ],
+                    "output_format": "pdf",
+                    "engine": "libreoffice",
+                    "pages": "1"
+                },
+                "export2url": {
+                    "operation": "export/url",
+                    "input": [
+                        "Convert2pdf"
+                    ],
+                    "inline": true,
+                    "archive_multiple_files": false
+                }
+            },
+            "tag": "jobbuilder"
+        }).then(async function (Job) {
+            console.log("Starting First Job")
+            let uploadTask = Job.tasks.find(task => task.name === 'upload-my-file');
+            let inputFile = fs.createReadStream(`${File}`);
+            await cloudConvert.tasks.upload(uploadTask, inputFile, File);
+            return Job;
+        }).then(async function (JobAfterUpload) {
+            console.log("JobAfterUpload.id >>> ", JobAfterUpload.id);
+            let PDFURL = await cloudConvertGetURL(JobAfterUpload.id);
+            resolve(PDFURL);
+        }).catch(function (e) {
+            console.error("Error2::", e.toString());
+            reject(e.toString());
+        });
+    })
+
+}
+
+const cloudConvertGetURL = function cloudConvertGetURL(JobID) {
+
+    return new Promise(async function (resolve, reject) {
+        let TaskURL = new URL(`https://sync.api.cloudconvert.com/v2/jobs/${JobID}`);
+
+        var options = {
+            "method": "GET",
+            "hostname": TaskURL.hostname,
+            "port": null,
+            "path": TaskURL.pathname,
+            "headers": {
+                "authorization": `Bearer ${CLOUD_CONVERT_API_KEY}`,
+                "cache-control": "no-cache",
+            }
+        };
+
+        try {
+            let idx = setInterval(function () {
+                var req = https.request(options, function (res) {
+                    var chunks = [];
+                    res.on("data", function (chunk) {
+                        chunks.push(chunk);
+                    });
+                    res.on("end", function () {
+                        let previewUrl = null
+                        var body = JSON.parse((Buffer.concat(chunks)).toString());
+                        let newjob = body.data.tasks.filter(function (el) {
+                            return el.name == 'export2url';
+                        });
+                        if (typeof JobID !== 'undefined' && typeof newjob !== 'undefined' && newjob && previewUrl == null && newjob[0].result != null) {
+                            previewUrl = newjob[0].result.files[0].url;
+                            clearInterval(idx);
+                            resolve(previewUrl);
+                        }
+                    });
+                });
+                req.end();
+            }, 500); // the interval when to call the function again 500ms = 0.5sek 
+        } catch (e) {
+            console.log(e.toString());
+            reject(e.toString());
+        }
+    });
+}
+
+const uploadToS3 = function uploadToS3(url, generalInfo, username) {
+
+    let type = 'application/pdf';
+
+    return axios.get(url, {
+        responseType: 'arraybuffer'
+    }).then(response => {
+        let buffer = Buffer.from(response.data, 'base64');
+        return (async () => {
+            var params = {
+                Key: `certs-${generalInfo.platform}/${generalInfo.service_id}/${generalInfo.user_id}-${username}.pdf`,
+                Body: buffer,
+                Bucket: S3_BUCKET,
+                ContentType: type,
+                ACL: 'public-read' //becomes a public URL
+            };
+
+            //notice use of the upload function, not the putObject function
+            return s3.upload(params).promise().then((response) => {
+                return response.Location;
+            }, (err) => {
+                return { type: 'error', err: err };
+            });
+        })();
+    }).catch(err => {
+        console.log(err)
+        return { type: 'error', err: err };
+    });
+}
 
 module.exports = {
-    downloadTemplate, modifyTemplate
+    downloadTemplate, modifyTemplate, cloudConvertToPDF, uploadToS3
 }
